25cd4eaa47f9e962785bce1eae9ffd7d36152158
fuyi <1406751952@qq.com>
2018-01-19 15:10:15 +0800

    oauth2

---
+++ security-oauth/src/main/java/com/fuyi/config/OAuth2ServerConfig.java
@@ -0,0 +1,97 @@
+package com.fuyi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * Created by Administrator on 2018/1/19 0019.
+ */
+@Configuration
+public class OAuth2ServerConfig {
+
+    private static final String DEMO_RESOURCE_ID = "order";
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+            resources.resourceId(DEMO_RESOURCE_ID).stateless(true);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .and()
+                    .requestMatchers().anyRequest()
+                    .and()
+                    .anonymous()
+                    .and()
+                    .authorizeRequests()
+
+                    // /order/* 必须被认证访问
+                    .antMatchers("/order/*").authenticated();
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Autowired
+        private RedisConnectionFactory redisConnectionFactory;
+
+        /**
+         * 配置 client(云冲印)在认证服务器侧注册的信息，
+         * 如：
+         * @param clients
+         * @throws Exception
+         */
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+                    .inMemory().withClient("client_1")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("select")
+                    .authorities("client")
+                    .secret("123456")
+                    .and().withClient("client_2")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .scopes("select")
+                    .authorities("client")
+                    .secret("123456");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .tokenStore(new RedisTokenStore(redisConnectionFactory))
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            security.allowFormAuthenticationForClients();
+        }
+    }
+}
